91237a44a63a4085b9c331598fa26f66f1d35894
Rachkem01 <122974865+Rachkem01@users.noreply.github.com>
2024-09-14 22:19:37 +0100

    Update Home.jsx

--- FrontEnd/Bookstore/src/pages/Home.jsx
+++ FrontEnd/Bookstore/src/pages/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
 
     useEffect(()=>{
         setLoading(true)
-        axios.get('https://bookstoree-y7zx.onrender.com/books')
+        axios.get('https://bookstore5.onrender.com/books')
         .then((response)=>{
             setBooks(response.data.data)
             setLoading(false)
